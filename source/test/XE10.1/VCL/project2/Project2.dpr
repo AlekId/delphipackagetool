cdaa26204dfb1c083a982e603c88790167a8e8dd
delphifreak71 <delphifreak71@9784c235-fe8a-404b-8088-45597e552109>
2017-04-06 21:25:34 +0000

    -added test-project for XE10.1.

---
+++ source/test/XE10.1/VCL/project2/Project2.dpr
@@ -0,0 +1,14 @@
+program Project2;
+
+uses
+  Vcl.Forms,
+  Unit1 in 'Unit1.pas' {Form11};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm11, Form11);
+  Application.Run;
+end.
